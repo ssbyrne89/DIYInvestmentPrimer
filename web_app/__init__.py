fa50707a3bf00e7d0497a4f043b3c41086197f1d
Sean Byrne <ssbyrne89@gmail.com>
2021-04-08 00:01:26 -0600

    refactored db

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,43 +2,43 @@
 
 from flask import Flask
 
-from web_app.models import db, migrate
+from web_app.models import db
 from web_app.routes.home_routes import home_routes
 from web_app.routes.company_routes import company_routes
-
-from web_app.database import DATABASE_URI, populateDB, \
-                            createAPICallTable, updateDatabase, dbExists
+from os import getenv
+#from web_app.database import populateDB, \
+#                            createAPICallTable, updateDatabase, dbExists
                                 
 
 from sqlalchemy import create_engine
 
 
-createAPICallTable()
+#createAPICallTable()
 
 # DATABASE_URI = "sqlite:///DIY_Investment_Primer_dev_DB.db" # using relative filepath
 #DATABASE_URI = "sqlite:////Users/Username/Desktop/your-repo-name/web_app_99.db" # using absolute filepath on Mac (recommended)
 #DATABASE_URI = "sqlite:///C:\Users\Sean\Documents\GitHub\DIYInvestmentPrimer\\DIY_Investment_Primer_dev_DB.db"
 # using absolute filepath on Windows (recommended) h/t: https://stackoverflow.com/a/19262231/670433
 
-engine = create_engine(DATABASE_URI, echo=False)
-sqlite_connection = engine.connect()
+#engine = create_engine(os.getenv("DATABASE_URI"), echo=False)
+#sqlite_connection = engine.connect()
 
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI 
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI") 
     db.init_app(app)
-    migrate.init_app(app, db)
+    #migrate.init_app(app, db)
 
     
-    if not dbExists():
-        populateDB()
-    else:
-        print("monthly dividend summary already exists!")
+    #if not dbExists():
+    #    populateDB()
+    #else:
+    #    print("monthly dividend summary already exists!")
 
-    updateDatabase() # will get moved when we update
+    #updateDatabase() # will get moved when we update
     
     app.register_blueprint(home_routes)
     app.register_blueprint(company_routes)
--- web_app/routes/company_routes.py
+++ web_app/routes/company_routes.py
@@ -1,29 +1,39 @@
 # web_app/routes/company_routes.py
-
+import pandas as pd
 from flask import Blueprint, jsonify, request, render_template #, flash, redirect
 
-from web_app.models import db, Month_Summary
+from web_app.models import *
 
 company_routes = Blueprint("company_routes", __name__)
 
-@company_routes.route("/companies.json")
-def list_companies():
-    """companies = [
-        {"id": 1, "title": "Company 1"},
-        {"id": 2, "title": "Company 2"},
-        {"id": 3, "title": "Company 3"},
-    ]
-    return jsonify(companies)"""
 
-    month_summary = Month_Summary(title=request.form["title"], author_id=request.form["author_name"])
-    db.session.add(new_book)
-    db.session.commit()
 
 @company_routes.route("/companies")
 def list_companies_for_humans():
-    companies = [
-        {"id": 1, "title": "Company 1"},
-        {"id": 2, "title": "Company 2"},
-        {"id": 3, "title": "Company 3"},
-    ]
-    return render_template("dividend_companies.html", message="Here's some companies", companies=companies)
+    return render_template("AllS&P500.html", message="Here's all the companies on the S&P 500",
+                            companies=get_AllCompanies())
+
+def get_AllCompanies():
+    all = Company_Info.query.all()
+    names = [record.Company_Name for record in all]
+    return names
+
+
+def createCompanyInfoTable():  # ran once
+    SandP500 = pd.read_csv('../DIYInvestmentPrimer/SandP_500_companies.csv')
+
+    for x in range(0, len(SandP500)):
+        db.create_all()
+        company_entry = Company_Info.query.get
+        (Company_Info(Company_Name=SandP500['Security'][x],
+                                     Company_Ticker=SandP500['Symbol'][x],
+                                     Sector=SandP500['GICS Sector'][x],
+                                     SubIndustry=SandP500['GICS Sub-Industry'][x],
+                                     HQ_Location=SandP500['Headquarters Location'][x],
+                                     Date_first_added_to_SP500=SandP500['Date first added'][x],
+                                     Founded=SandP500['Founded'][x]))
+
+        db.session.add(company_entry)
+        db.session.commit()
+
+    
