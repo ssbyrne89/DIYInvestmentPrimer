4e2f44c19d6bf1be795ed5d40e58eff964b34716
Sean Byrne <ssbyrne89@gmail.com>
2021-04-28 23:08:04 -0600

    free download funtion added; works on chrome browser but not brave browser

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/home_routes.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_file, abort
 
 
 
@@ -22,6 +22,19 @@ def wtmm():
 @home_routes.route("/real_estate_resources")
 def REresources():
     return render_template("REresources.html")
+
+@home_routes.route("/real_estate_resources/freeColoringBook")
+def freeColoringBook():
+    return render_template("coloringBook.html")
+
+@home_routes.route("/real_estate_resources/getREcoloringBook")
+def getColoringBook():
+    """when tested, this download works with the google chrome
+    browser but not with the Brave browser
+    """
+    path="templates\static\Sellers_guide_coloring_book.pdf"
+    return send_file(path, as_attachment=True,
+    attachment_filename="free_real_estate_coloring_book")
 
 @home_routes.route("/personal_finance_and_savings_resources")
 def PFSresources():
